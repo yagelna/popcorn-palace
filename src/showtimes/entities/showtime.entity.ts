51136c9a566f4d7d061d4c08d7ba879445ccc76b
Yagel Nahshon <yagelnahshon@gmail.com>
2025-03-20 20:03:30 +0200

    complete basic CRUD for showtimes module

--- src/showtimes/entities/showtime.entity.ts
+++ src/showtimes/entities/showtime.entity.ts
@@ -4,26 +4,26 @@ import { Exclude } from "class-transformer";
 
 @Entity( { name: 'showtimes' } )
 export class Showtime {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Movie, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'movie' })
+  @Exclude({ toPlainOnly: true }) // לא להציג את האובייקט movie בפלט
+  movie: Movie;
   
-    @ManyToOne(() => Movie, { onDelete: 'CASCADE' })
-    @JoinColumn({ name: 'movie' })
-    @Exclude({ toPlainOnly: true }) // לא להציג את האובייקט movie בפלט
-    movie: Movie;
-    
-    @RelationId((showtime: Showtime) => showtime.movie)
-    movieId: number;
+  @RelationId((showtime: Showtime) => showtime.movie)
+  movieId: number;
 
-    @Column()
-    theater: string;
+  @Column()
+  theater: string;
 
-    @Column({ name: 'start_time' })
-    startTime: Date;
+  @Column({ name: 'start_time' })
+  startTime: Date;
 
-    @Column({ name: 'end_time' })
-    endTime: Date;
+  @Column({ name: 'end_time' })
+  endTime: Date;
 
-    @Column('float')
-    price: number;
-  }
+  @Column('float')
+  price: number;
+}
