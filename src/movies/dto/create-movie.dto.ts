342a9ffca345dc8af9f020e2c597ffa89618ba57
Yagel Nahshon <yagelnahshon@gmail.com>
2025-03-21 17:06:12 +0200

    add IsNotEmpty decorator

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,19 +1,24 @@
-import { IsString, IsInt, IsNumber, Min } from 'class-validator';
+import { IsString, IsInt, IsNumber, Min, IsNotEmpty } from 'class-validator';
 
 export class CreateMovieDto {
   @IsString()
+  @IsNotEmpty()
   title: string;
 
   @IsString()
+  @IsNotEmpty()
   genre: string;
 
   @IsInt()
   @Min(1)
+  @IsNotEmpty()
   duration: number;
 
   @IsNumber()
+  @IsNotEmpty()
   rating: number;
 
   @IsInt()
+  @IsNotEmpty()
   releaseYear: number;
 }
